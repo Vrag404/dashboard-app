3ca93a47ace171e1bc67bb012f5cf8c3e65647be
Vrag404 <vrag404@protonmail.com>
2022-11-09 12:14:08 -0300

    Adding cors and types for headers configuration

--- server-side/src/server.ts
+++ server-side/src/server.ts
@@ -1,18 +1,17 @@
 import express, { Application } from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 
 import RouteControllers from './routes/routes';
 
 const server: Application = express();
 
-// Settings
 server.set('port', 3005 || process.env.PORT);
 
-// Middlewares
 server.use(express.json());
 server.use(morgan('dev'));
+server.use(cors());
 
-// Routes
 server.use('/api/', RouteControllers);
 
 export default server;
