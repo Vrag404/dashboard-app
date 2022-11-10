4f9b516717630e9bc0a3a6303237e608d23ddc2a
Vrag404 <vrag404@protonmail.com>
2022-11-10 13:47:45 -0300

    Indentation correction

--- client-side/src/main.tsx
+++ client-side/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { AuthProvider } from './contexts/authContext'
 import RoutesController from './routes/RoutesController'
 
 import GlobalStyle from './styles/Global'
@@ -7,8 +8,14 @@ import GlobalStyle from './styles/Global'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <>
+
       <GlobalStyle />
-      <RoutesController />
+
+      <AuthProvider>
+        <RoutesController />
+      </AuthProvider>
+      
     </>
+
   </React.StrictMode>,
 )
