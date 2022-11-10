43139944caac91ef771400958b485d276cd47c33
Vrag404 <vrag404@protonmail.com>
2022-11-10 13:49:51 -0300

    Identation correction.

--- client-side/src/routes/RoutesController.tsx
+++ client-side/src/routes/RoutesController.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import Home from '../pages/Home/Home'
 import Signin from '../pages/Signin/Signin'
 import Signup from '../pages/Signup/Signup'
@@ -6,11 +7,15 @@ import Signup from '../pages/Signup/Signup'
 const RoutesController = () => {
   return (
     <BrowserRouter>
+
       <Routes>
+
         <Route path="/" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<Home />} />
+
       </Routes>
+      
     </BrowserRouter>
   )
 }
