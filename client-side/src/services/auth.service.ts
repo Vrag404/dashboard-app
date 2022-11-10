e272f82746ad2a5c9d194293fd7b76ce4d4d80ba
Vrag404 <vrag404@protonmail.com>
2022-11-10 13:50:29 -0300

    Creating a service for authentication.

---
+++ client-side/src/services/auth.service.ts
@@ -0,0 +1,26 @@
+import { ISignin, ISignup } from "../types/auth.interfaces"
+
+import api from "./api.service";
+
+const API_URL: string = import.meta.env.VITE_API_URL as string;
+
+export const signin = async ({ email, password }: ISignin) => {
+  return await api.post(API_URL + 'signin', {
+      email: email,
+      password: password,
+    }).then(res => {
+      localStorage.setItem('auth-token', res.data.token);
+    })
+}
+
+export const signup = async ({ username, email, password }: ISignup) => {
+  return await api.post(API_URL + 'signup', {
+      username: username,
+      email: email,
+      password: password,
+    });
+}
+
+export const logout = () => {
+  localStorage.removeItem("auth-token");
+};
