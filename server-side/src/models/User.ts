b9e2f1e4485233c1717c6ae29b35a58d6d8a987f
Vrag404 <vrag404@protonmail.com>
2022-11-24 18:02:14 -0300

    Adding contact references to the users schema.

--- server-side/src/models/User.ts
+++ server-side/src/models/User.ts
@@ -2,40 +2,46 @@ import { Schema, model, Document } from 'mongoose';
 import bcrypt from 'bcrypt';
 
 export interface IUser extends Document {
-    username: string;
-    email: string;
-    password: string;
+  username: string;
+  email: string;
+  password: string;
 
-    encryptPassword(password: string): Promise<string>;
-    validatePassword(password: string): Promise<boolean>;
+  encryptPassword(password: string): Promise<string>;
+  validatePassword(password: string): Promise<boolean>;
 };
- 
+
 const UserSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        min: 4,
-    },
-    email: {
-        type: String,
-        unique:  true,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
+  username: {
+    type: String,
+    required: true,
+    min: 4,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  contacts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Contact"
     }
+  ]
 }, {
-    timestamps: true
+  timestamps: true
 });
 
 UserSchema.methods.encryptPassword = async (password: string): Promise<string> => {
-    const salt = await bcrypt.genSalt(10);
-    return bcrypt.hash(password, salt);
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hash(password, salt);
 };
 
 UserSchema.methods.validatePassword = async function (password: string): Promise<boolean> {
-    return await bcrypt.compare(password, this.password);
+  return await bcrypt.compare(password, this.password);
 };
 
 export default model<IUser>('User', UserSchema);
