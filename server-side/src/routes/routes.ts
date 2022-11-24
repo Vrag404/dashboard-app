4f2766ee543867116a9a4d69bff15bad0bf14ffd
Vrag404 <vrag404@protonmail.com>
2022-11-24 18:02:28 -0300

    Creating routes for contacts.

--- server-side/src/routes/routes.ts
+++ server-side/src/routes/routes.ts
@@ -1,9 +1,14 @@
+import { 
+  deleteContact,
+  getSingleContact,
+  updateContact 
+} from './../controllers/contactController';
+
 import { Router } from "express";
 
-import { signup, signin } from '../controllers/authController';
-import { home } from '../controllers/privateController';
-
+import { getAllContacts, createContact } from '../controllers/contactController';
 import { TokenValidation } from '../middlewares/authMiddleware';
+import { signup, signin } from '../controllers/authController';
 
 const router = Router();
 
@@ -11,7 +16,11 @@ const router = Router();
 router.post('/signup', signup);
 router.post('/signin', signin);
 
-// Private
-router.get('/home', TokenValidation, home)
+// Private Routes (Contact)
+router.get('/getContacts', TokenValidation, getAllContacts);
+router.get('/getContact/:id', TokenValidation, getSingleContact);
+router.post('/createContact', TokenValidation, createContact);
+router.put('/updateContact/:id', TokenValidation, updateContact);
+router.delete('/deleteContact/:id', TokenValidation, deleteContact);
 
 export default router;
