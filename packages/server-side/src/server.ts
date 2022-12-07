fc16b1b7e112c8e60f111e9d595b2e4ae4d36a39
Vrag404 <vrag404@protonmail.com>
2022-12-07 16:00:52 -0300

    Adding cors and headers configuration

--- packages/server-side/src/server.ts
+++ packages/server-side/src/server.ts
@@ -5,14 +5,29 @@ import cors from 'cors';
 import RouteControllers from './routes/routes';
 
 const PORT: string = (process.env.PORT as string);
+const API_URL: string = (process.env.API_URL as string);
 
 const server: Application = express();
+
+const options: cors.CorsOptions = {
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-Access-Token',
+  ],
+  credentials: true,
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  origin: API_URL,
+  preflightContinue: false,
+};
 
 server.set('port', PORT || 3015);
 
 server.use(express.json());
 server.use(morgan('dev'));
-server.use(cors());
+server.use(cors(options));
 
 server.use('/api/', RouteControllers);
 
